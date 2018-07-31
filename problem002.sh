7baa650023657972018029d0107f6be695f6fe3f
Marlon Monzon <martianworm17@gmail.com>
2018-07-31 15:46:25 +0800

    Remove commented code block

--- problem002.sh
+++ problem002.sh
@@ -5,17 +5,6 @@ var1=1;
 var2=2;
 var3=0;
 
-#for (( num=1; num<4000000; num++))
-#do
-#	
-#	if [ $((num%3)) == 0 ] || [ $((num%5)) == 0 ]
-#		then
-#		(( sum = sum + num ))
-#	fi
-#
-#done
-#echo "$sum"
-
 while [ $sum -lt 100 ]
 do
 
